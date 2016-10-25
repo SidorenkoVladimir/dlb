cf8068cb473717a1ef8d06cdef21731b87962081
BOB <sidorqq@gmail.com>
2016-10-25 21:41:25 +0400

    Revert "hz-sho"
    This reverts commit 38a513a1556f1ef8eb9914e6b1bb8c470e6c81c4.

--- wp-activate.php
+++ wp-activate.php
@@ -8,7 +8,7 @@
 
 define( 'WP_INSTALLING', true );
 
-/** Sets up the WordPress Environment. *///апра
+/** Sets up the WordPress Environment. */
 require( dirname(__FILE__) . '/wp-load.php' );
 
 require( dirname( __FILE__ ) . '/wp-blog-header.php' );
